5075c27ba3b258d600efd372ec440e6dcebf9a0c
Scott Robertson <scottymeuk@gmail.com>
2021-01-16 13:55:55 +0000

    Remove fake test

--- test/tokenable_test.rb
+++ test/tokenable_test.rb
@@ -6,8 +6,4 @@ class TokenableTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Tokenable::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
