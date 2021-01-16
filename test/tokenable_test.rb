c0b589f3c2b0b77ee7220ddb0760c1eb35cbd258
Scott Robertson <scottymeuk@gmail.com>
2021-01-16 16:29:52 +0000

    Adding rspec

--- test/tokenable_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class TokenableTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Tokenable::VERSION
-  end
-end
