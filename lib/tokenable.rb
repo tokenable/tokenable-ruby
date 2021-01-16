7208bf5ef2aa26cc0ce555a4792c227fe9814747
Marc Qualie <marc@marcqualie.com>
2021-01-16 17:08:20 +0000

    No need to include jwt

--- lib/tokenable.rb
+++ lib/tokenable.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-require 'jwt'
-
 require_relative 'tokenable/version'
 require_relative 'tokenable/authable'
 require_relative 'tokenable/railtie' if defined?(Rails)
 
 module Tokenable
   class Error < StandardError; end
+
   class Unauthorized < Error; end
 end
