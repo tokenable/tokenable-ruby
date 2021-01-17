9e518d754ef964d55def823529ac15f070684e4d
Scott Robertson <scottymeuk@gmail.com>
2021-01-17 19:11:31 +0000

    Rubocop fixes

--- lib/generators/tokenable/install_generator.rb
+++ lib/generators/tokenable/install_generator.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module Tokenable
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../../templates', __FILE__)
+      source_root File.expand_path('../templates', __dir__)
 
       def install
-        template "tokenable.rb.erb", 'config/initializers/tokenable.rb'
+        template 'tokenable.rb.erb', 'config/initializers/tokenable.rb'
         route "mount Tokenable::Engine => '/api/auth'"
       end
     end
