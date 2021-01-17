21653d7d323c806a75bda4f67a92c4a50777e3b0
Scott Robertson <scottymeuk@gmail.com>
2021-01-17 21:34:09 +0000

    Adding Sorcery

---
+++ lib/tokenable/strategies/sorcery.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Tokenable
+  module Strategies
+    module Sorcery
+      extend ActiveSupport::Concern
+
+      class_methods do
+        def from_tokenable_params(params)
+          email, password = parse_auth_params(params)
+
+          user = User.find_by(email: email)
+          return nil unless user
+
+          return nil unless user.valid_password?(password)
+
+          user
+        end
+
+        private
+
+        def parse_auth_params(params)
+          [
+            params.require(:email),
+            params.require(:password),
+          ]
+        end
+      end
+    end
+  end
+end
