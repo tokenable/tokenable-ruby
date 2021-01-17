3d2c8056b49b210ae10eed91bedc5c5de49b3c53
Scott Robertson <scottymeuk@gmail.com>
2021-01-17 21:02:47 +0000

    Suggest a list of strats

--- lib/generators/tokenable/install_generator.rb
+++ lib/generators/tokenable/install_generator.rb
@@ -17,11 +17,13 @@ module Tokenable
       end
 
       def setup_statergy
-        if options.strategy.in?(list_of_strategies)
-          strategy_class = options.strategy.classify
-          inject_into_file "app/models/#{file_name}.rb", "  include Tokenable::Strategies::#{strategy_class}\n", after: " < ApplicationRecord\n"
-        else
-          say 'Stargery not found'
+        if options.strategy
+          if options.strategy.in?(list_of_strategies)
+            strategy_class = options.strategy.classify
+            inject_into_file "app/models/#{file_name}.rb", "  include Tokenable::Strategies::#{strategy_class}\n", after: " < ApplicationRecord\n"
+          else
+            say "Stargery not found (#{options.strategy}). Available: #{list_of_strategies.join(', ')}"
+          end
         end
       end
 
@@ -31,6 +33,7 @@ module Tokenable
         Dir.entries(File.expand_path('../../tokenable/strategies', __dir__))
            .reject { |f| File.directory?(f) }
            .map { |f| File.basename(f, File.extname(f)) }
+           .compact
       end
     end
   end
