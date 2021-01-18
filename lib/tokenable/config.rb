cb50565af0744381df9daed9c64891733bca6eae
Scott Robertson <scottymeuk@gmail.com>
2021-01-18 00:09:34 +0000

    Setting the default lifespan here too

--- lib/tokenable/config.rb
+++ lib/tokenable/config.rb
@@ -3,8 +3,9 @@
 module Tokenable
   class Config
     # How long should the token last before it expires?
-    # E.G: Tokenable::Config.lifespan = 7.days
-    mattr_accessor :lifespan, default: nil
+    # E.G: Tokenable::Config.lifespan = 14.days
+    # You could set this to nil to disable expiring keys
+    mattr_writer :lifespan, default: -> { 7.days }
 
     # The secret used by JWT to encode the Token.
     # We default to Rails secret_key_base
