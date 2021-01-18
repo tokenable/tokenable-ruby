9264280909114a4a0313458bf1ed6e54d5704d07
Scott Robertson <scottymeuk@gmail.com>
2021-01-18 00:42:11 +0000

    Testing method_missing, and setters are working in Config

--- spec/tokenable/config_spec.rb
+++ spec/tokenable/config_spec.rb
@@ -8,4 +8,20 @@ describe Tokenable::Config do
     expect(Tokenable::Config.secret).to eq(Rails.application.secret_key_base)
     expect(Tokenable::Config.lifespan).to eq(7.days)
   end
+
+  it 'ensures setters are working' do
+    expect(Tokenable::Config.secret).to eq(Rails.application.secret_key_base)
+
+    random_string = SecureRandom.hex
+    Tokenable::Config.secret = random_string
+    expect(Tokenable::Config.secret).to eq(random_string)
+  end
+
+  it 'ensures method_missing is working as expected' do
+    expect(Tokenable::Config.respond_to?(:secret)).to eq(true)
+    expect(Tokenable::Config.secret).to eq(Rails.application.secret_key_base)
+
+    expect{Tokenable::Config.not_a_thing}.to raise_error(NoMethodError)
+    expect(Tokenable::Config.respond_to?(:not_a_thing)).to eq(false)
+  end
 end
