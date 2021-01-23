05f4aba1eb9554ebdfd5c42ecd74dd4fc612cba5
Scott Robertson <scottrobertson@users.noreply.github.com>
2021-01-23 19:18:45 +0000

    user_class should be a string if possible to avoid a Rails deprecation warning (#24)

--- spec/tokenable/config_spec.rb
+++ spec/tokenable/config_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 
 describe Tokenable::Config do
   it 'has the default values' do
+    expect(described_class.user_class).to eq(User)
     expect(described_class.secret).not_to eq(nil)
     expect(described_class.secret).to eq(Rails.application.secret_key_base)
     expect(described_class.lifespan).to eq(7.days)
@@ -23,5 +24,12 @@ describe Tokenable::Config do
 
     expect { described_class.not_a_thing }.to raise_error(NoMethodError)
     expect(described_class.respond_to?(:not_a_thing)).to eq(false)
+  end
+
+  it 'ensures user_class returns a class, when a string is defined' do
+    described_class.user_class = 'User'
+
+    expect(described_class.class_variable_get('@@user_class')).to be_a(String)
+    expect(described_class.user_class).to eq(User)
   end
 end
